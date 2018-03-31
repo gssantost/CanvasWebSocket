3c3391bcd788ba1915ccfb1bf6a168fe10fd727d
FabiolaLombardi <37227394+FabiolaLombardi@users.noreply.github.com>
2018-03-30 21:37:13 -0400

    Colors by user + bootstrap

--- WebContent/app.js
+++ WebContent/app.js
@@ -5,17 +5,19 @@ console.log("Whiteboard is ready");
         click: false,
         move: false
     }
+
     let coords = {
         prevX: 0, prevY:0,
-        x:0, y:0
-    }    
+        x:0, y:0,
+        
+    }      
 const canvas = $("whiteboard");
 const ctx = canvas.getContext("2d");
 const width = window.innerWidth;
 const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
-//Eventos del lienzo
+// Eventos del lienzo
 canvas.onmousedown = e => {
     mouse.click = true;
     coords.prevX = e.pageX - canvas.offsetLeft;
@@ -29,36 +31,54 @@ canvas.onmousemove = e => {
     if (mouse.click) {
         coords.x = e.pageX - canvas.offsetLeft;
         coords.y = e.pageY - canvas.offsetTop;
-        ws.send(JSON.stringify(coords)); //envío objeto de coordenadas a través del WebSocket
+        ws.send(JSON.stringify(coords)); // envío objeto de coordenadas a
+											// través del WebSocket
     }
 }
+
+
 const draw = (line) => {
+	let col = $("jscolor").value;
+	console.log(col);
     ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.lineJoin = "round";
+    ctx.strokeStyle= "#" + col;
+	// var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+	// ctx.strokeStyle= colors[Math.floor(Math.random() * colors.length)];
+    // ctx.strokeStyle= "#"+((1<<24)*Math.random()|0).toString(16);
     ctx.moveTo(line.prevX, line.prevY);
     ctx.lineTo(line.x, line.y);
     ctx.stroke();
     coords.prevX = line.x;
     coords.prevY = line.y;
 }
-//Conexión al WebSocket
+
+
+
+// Conexión al WebSocket
 let ws;
+let colr = $("jscolor").value;
 const connect = () => {
     ws = new WebSocket("ws://" + location.host + "/CanvasWebSocket/paint/" + $("username").value);
 
     ws.onmessage = e => {
-        //console.log(JSON.parse(e.data));
+        // console.log(JSON.parse(e.data));
         let line = JSON.parse(e.data);
         draw(line);
-        let users = JSON.parse(e.data).usrs;
-        for (let key in users) {
-            $("users").innerHTML += `${users[key]} is online...!<br>`;
-        }
+       let users = JSON.parse(e.data).usrs;
+       		  for (let key in users) { 
+       			  $("users").innerHTML += `${users[key]} is online...!<br>`; 
+       		  }
     }
-
+    
     ws.onopen = e => {
         console.log("Connected!");
+      	if($("jscolor").value === "FFFFFF"){
+      		alert("Estas a punto de pintar en blanco, si deseas, puedes cambiar el color!");
+      	}else{
+      		alert("Empieza a pintar!");
+      	}
     }
     
     ws.onclose = e => {
@@ -67,11 +87,24 @@ const connect = () => {
             console.log(JSON.parse(e.data));
             ws.close();
         }
+        
+     ws.onerror = e => {
+    	 console.log("Ha habido un error en la comunicacion",e.data);
+     }   
     }
 }
 const logout = () => {
     ws.close();
+    console.log("Haz Join! para dibujar");
+    alert("vuelve pronto!")
 }
-//Event Listeners
+
+const clear = () =>{
+	const canvas = $("whiteboard");
+	const ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, width, height);
+}
+// Event Listeners
 $("connectBtn").addEventListener("click", connect);
 $("disconnectBtn").addEventListener("click", logout);
+$("clearBtn").addEventListener("click", clear);
--- src/SharedPaint.java
+++ src/SharedPaint.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -12,7 +13,6 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 @ServerEndpoint("/paint/{username}")
@@ -20,22 +20,23 @@ public class SharedPaint {
 	private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
 	private static HashMap<String, String> users = new HashMap<>();
 	private static JSONArray drawingHistory = new JSONArray();
-	//private static JSONArray usersHistory = new JSONArray();
-	
+	// private static JSONArray usersHistory = new JSONArray();
+
 	@OnOpen
 	public void onOpen(Session session, @PathParam("username") String username) throws IOException {
 		clients.add(session);
 		users.put(session.getId(), username);
-		if (drawingHistory.length() > 0) {
-			for (int i = 0; i < drawingHistory.length(); i++) {
-				broadcast(drawingHistory.getJSONObject(i).toString());
-			}
-		}
+		/*
+		 * if (drawingHistory.length() > 0) { for (int i = 0; i <
+		 * drawingHistory.length(); i++) {
+		 * broadcast(drawingHistory.getJSONObject(i).toString()); } }
+		 */
 		JSONObject data = new JSONObject();
 		data.put("usrs", users);
 		broadcast(data.toString());
+		System.out.println(data);
 	}
-	
+
 	@OnClose
 	public void onClose(Session session) throws IOException {
 		JSONObject data = new JSONObject();
@@ -44,21 +45,29 @@ public class SharedPaint {
 		users.remove(session.getId());
 		clients.remove(session);
 		broadcast(data.toString());
+		System.out.println(data);
 	}
-	
+
 	@OnMessage
 	public void onMessage(Session session, String cords) throws IOException {
 		JSONObject data = new JSONObject(cords);
-		//JSONObject row = new JSONObject();
-		//row.put("line", data);
 		drawingHistory.put(data);
 		broadcast(data.toString());
 		System.out.println(data);
 	}
-	
+
+	@OnError
+	public void onError(Throwable e) throws IOException {
+		e.printStackTrace();
+		JSONObject data = new JSONObject();
+		data.put("status", 500).put("msg", e.getStackTrace());
+		broadcast(data.toString());
+		System.out.println(data);
+	}
+
 	public static void broadcast(String response) throws IOException {
-		synchronized(clients) {
-			for (Session client: clients) {
+		synchronized (clients) {
+			for (Session client : clients) {
 				client.getBasicRemote().sendText(response);
 			}
 		}
